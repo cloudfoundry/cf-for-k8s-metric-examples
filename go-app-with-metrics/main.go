09cd4aeb89b2a318b3de75e459fd6ab9a96e437c
David Timm <dtimm@vmware.com>
2020-07-09 19:35:51 +0000

    add app_test_metric by default
    [#173472811]
    
    Signed-off-by: Louie Brann <lbrann@vmware.com>

--- go-app-with-metrics/main.go
+++ go-app-with-metrics/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"net/http"
+	"time"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var customMetric = promauto.NewCounter(prometheus.CounterOpts{
+	Name: "app_test_metric",
+	Help: "The number of seconds since launch",
+})
+
+func incrementMetric() {
+	for {
+		customMetric.Inc()
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
+	go incrementMetric()
+
 	// Hosting default prometheus handler on :2112/metrics
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
