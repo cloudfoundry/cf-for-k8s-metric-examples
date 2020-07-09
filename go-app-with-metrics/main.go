c4b825c2cb1e89bdb4ff7f70b04c5f5703906612
David Timm <dtimm@vmware.com>
2020-07-09 23:02:36 +0000

    metrics for query params
    [#173203619]
    
    Signed-off-by: Louie Brann <lbrann@vmware.com>

--- go-app-with-metrics/main.go
+++ go-app-with-metrics/main.go
@@ -9,14 +9,14 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
-var customMetric = promauto.NewCounter(prometheus.CounterOpts{
+var customMetric = promauto.NewCounterVec(prometheus.CounterOpts{
 	Name: "app_test_metric",
 	Help: "The number of seconds since launch",
-})
+}, []string{"query"})
 
 func incrementMetric() {
 	for {
-		customMetric.Inc()
+		customMetric.WithLabelValues("").Inc()
 		time.Sleep(time.Second)
 	}
 }
@@ -25,6 +25,15 @@ func main() {
 	go incrementMetric()
 
 	// Hosting default prometheus handler on :2112/metrics
-	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/metrics", queryParamWrapper(promhttp.Handler()))
 	http.ListenAndServe(":2112", nil)
+}
+
+func queryParamWrapper(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for k, _ := range r.URL.Query() {
+			customMetric.WithLabelValues(k).Inc()
+		}
+		h.ServeHTTP(w, r)
+	})
 }
